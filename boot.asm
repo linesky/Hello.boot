e460751790c7bce967063d091cfb2d81c0faa224
sky line <32512284+linesky@users.noreply.github.com>
2024-04-26 18:47:04 +0100

    Add files via upload

--- boot.asm
+++ boot.asm
@@ -3,6 +3,15 @@ org 0x7c00
 boots:
 jmp _start
 _start:
+    push ds
+    mov ax,0xb800
+    mov si,1
+    mov ds,ax
+    mov al,0x67
+    mov cx,2000
+    mov dx,2
+    call fill
+    pop ds
     mov si,hello
     call puts
 hltas:
@@ -20,9 +29,19 @@ putc:
     mov bl,0x7
     int 0x10
 ret
-
+fill:
+    cmp cx,0
+    jz fill3
+    fill2:
+        ds
+        mov [si],al
+        add si,dx
+        dec cx
+        jnz fill2
+fill3:
+ret        
 hello:
-db "hello world.\n",0,0
+db "hello world.",13,10,0,0
 spere:
 times  510-(spere-boots) db 0 
 signature:
