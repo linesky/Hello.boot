f4b6bea5559df33df8a9565d97f8dd9a1f7d75cb
sky line <32512284+linesky@users.noreply.github.com>
2024-04-27 14:35:41 +0100

    Add files via upload

--- make.sh
+++ make.sh
@@ -1,7 +1,8 @@
-printf "\x1bc\x1b[43;37m"
+printf "\x1bc\x1b[43;37m give me the .c file name of kernel ? "
+read a1
 nasm -o /tmp/boot.bin boot.asm
 bcc -c -Md libdos.c -o libdos.a
-bcc -x -i -L -Md hello.c -o hello.com
+bcc -x -i -L -Md "$a1" -o hello.com
 cp -f /tmp/boot.bin ./
 dd if=./hello.com of=./boot.bin seek=1 conv=notrunc
 /usr/bin/qemu-system-x86_64 -boot a -fda boot.bin
